a818a61706abc27e2aac19ae8fec727c7e7b7711
aloisdg <aloisdegouvello@live.fr>
2016-02-20 18:49:52 +0530

    Implement MVVM pattern

---
+++ SeasonPaper/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeasonPaper.ViewModel
+{
+	public class MainViewModel
+	{
+		public string Message { get; set; }
+
+		public MainViewModel()
+		{
+			Message = "Test Message";
+		}
+	}
+}
